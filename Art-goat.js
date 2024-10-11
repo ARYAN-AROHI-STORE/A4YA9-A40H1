42ec2223ce9d1cf438bf5803ffaf5bbc8f5c883e
ARYAN-AROHI-STORE <aryan.arohi@yahoo.com>
2024-10-11 20:51:13 +0600

    Update Art-goat.js

--- Art-goat.js
+++ Art-goat.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 const baseApiUrl = async () => {
   const base = await axios.get(
-    `https://raw.githubusercontent.com/Blankid018/D1PT0/main/baseApiUrl.json`
+    `https://raw.githubusercontent.com/ARYAN-AROHI-STORE/A4YA9-A40H1/refs/heads/main/APIRUL.json`
   );
   return base.data.api;
 };
