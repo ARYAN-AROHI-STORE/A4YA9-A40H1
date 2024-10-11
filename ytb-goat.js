bb6c0be5fdc33d83579abd8fccea619bc3d4f306
ARYAN-AROHI-STORE <aryan.arohi@yahoo.com>
2024-10-11 21:25:36 +0600

    Update ytb-goat.js

--- ytb-goat.js
+++ ytb-goat.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 
 const baseApiUrl = async () => {
 	const base = await axios.get(
-`https://raw.githubusercontent.com/Blankid018/D1PT0/main/baseApiUrl.json`
+`https://raw.githubusercontent.com/ARYAN-AROHI-STORE/A4YA9-A40H1/refs/heads/main/APIRUL.json`
 	);
 	return base.data.api;
 };
